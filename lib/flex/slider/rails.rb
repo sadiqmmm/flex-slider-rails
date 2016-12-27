8ca13cb7b127b41aa00d885f546996708e5a5f52
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-12-27 17:40:17 +0530

    test v2

--- lib/flex/slider/rails.rb
+++ lib/flex/slider/rails.rb
@@ -2,8 +2,19 @@ require "flex/slider/rails/version"
 
 module Flex
   module Slider
-    module Rails
-      class Engine < ::Rails::Engine; end
+    module Rails      
+       class Engine < ::Rails::Engine
+        initializer :append_dependent_assets_path, :group => :all do |app|
+        app.config.assets.paths += %w( fonts )
+
+        app.config.assets.precompile += %w( flexslider.css )
+        app.config.assets.precompile += %w( jquery.flexslider.js )
+
+        app.config.assets.precompile += %w( flexslider-icon.eot )
+        app.config.assets.precompile += %w( flexslider-icon.svg )
+        app.config.assets.precompile += %w( flexslider-icon.ttf )
+        app.config.assets.precompile += %w( flexslider-icon.woff )
+      end
     end
   end
 end
