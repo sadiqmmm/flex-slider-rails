22bb1abe76c77d673049ee6c122685032bb402a6
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-12-27 16:30:58 +0530

    Initial commit

---
+++ lib/flex/slider/rails.rb
@@ -0,0 +1,9 @@
+require "flex/slider/rails/version"
+
+module Flex
+  module Slider
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/flex/slider/rails/version.rb
@@ -0,0 +1,7 @@
+module Flex
+  module Slider
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
