a61fda00cbde4f4c3c0c6c621fd77d194e6524cf
Mohammed Sadiq <sadiqmmm@gmail.com>
2017-09-24 19:33:42 +0530

    Bug fix css and font v2.6.4.1

--- lib/flex/slider/rails.rb
+++ lib/flex/slider/rails.rb
@@ -7,7 +7,7 @@ module Flex
         initializer :append_dependent_assets_path, :group => :all do |app|
           app.config.assets.paths += %w( fonts )
 
-          app.config.assets.precompile += %w( flexslider.css )
+          app.config.assets.precompile += %w( flexslider.css.scss )
           app.config.assets.precompile += %w( jquery.flexslider.js )
 
           app.config.assets.precompile += %w( flexslider-icon.eot )
